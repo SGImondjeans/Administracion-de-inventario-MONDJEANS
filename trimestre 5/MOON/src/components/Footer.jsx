3bc3cbd2fd35f960e7f5060620dd191fae10bdff
SGImondjeans <proyectomondjeans@gmail.com>
2026-03-18 21:55:29 -0500

    Limpieza del repositorio, solo se deja PROYECTO1

--- trimestre 5/MOON/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-import "../css/Footer.css"
-export default function Footer() {
-  return (
-    <footer className="bg-dark text-white text-center py-3">
-      Mond Jeans
-    </footer>
-  );
-}
--- trimestre 5/MOON/src/pages/AdminPage.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import AdminPanel from "../components/AdminPanel";
-
-export default function AdminPage() {
-  return <AdminPanel />;
-}
